4358e54b5167721d61dd7a2a732e10f2c7c7787f
baoj3101 <38962382+baoj3101@users.noreply.github.com>
2019-04-19 16:38:28 -0700

    Update BallTestTwo.java

--- src/pong/BallTestTwo.java
+++ src/pong/BallTestTwo.java
@@ -1,69 +1,51 @@
-package pong;
-
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Canvas;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
-class BallTestTwo extends Canvas implements Runnable
-{
-	private Ball ball;
-        
-	public BallTestTwo()
-	{
-		setBackground(Color.WHITE);
-		setVisible(true);
+class BallTestTwo extends Canvas implements Runnable {
+    private Ball ball;
 
-		//instantiate a new Ball
-                ball = new Ball(100,100,30,50,Color.BLUE,5,6);
+    public BallTestTwo() {
+        setBackground(Color.WHITE);
+        setVisible(true);
+
+        ball = new Ball();
 
 
-		//test the Ball thoroughly
-		
-
-		//test all constructors
+        //test the Ball thoroughly
 
 
-		new Thread(this).start();
-	}
-	
-	public void update(Graphics window)
-	{
-		paint(window);
-	}
+        //test all constructors
 
-	public void paint(Graphics window)
-	{
-		ball.moveAndDraw(window);
 
-		if(!(ball.getxPos()>=10 && ball.getxPos()<= 550 - ball.getWidth()))
-		{
-			ball.setxSpeed(-ball.getxSpeed());
-		}
+        new Thread(this).start();
+    }
 
-		if(!(ball.getyPos()>=10 && ball.getyPos()<= 450 - ball.getHeight()))
-		{
-			ball.setySpeed(-ball.getySpeed());
-		}
-	}
-	
-   public void run()
-   {
-   	try
-   	{
-   		while(true)
-   		{
-   		   Thread.currentThread().sleep(19);
-            repaint();
-         }
-      }catch(Exception e)
-      {
-      }
-  	}	
+    public void update(Graphics window) {
+        paint(window);
+    }
+
+    public void paint(Graphics window) {
+        ball.moveAndDraw(window);
+
+        if (!(ball.getX() >= 10 && ball.getX() <= 550)) {
+            ball.setXSpeed(-ball.getXSpeed());
+        }
+
+        if (!(ball.getY() >= 10 && ball.getY() <= 450)) {
+            ball.setYSpeed(-ball.getYSpeed());
+        }
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(19);
+                repaint();
+            }
+        } catch (Exception e) {
+        }
+    }
 }
